0b766818eef8e1a066df53c825fba2fef41fac10
rhys5690 <rhys5690@gmail.com>
2018-05-25 14:47:09 +1000

    added app generation functionality

--- generators/app/index.js
+++ generators/app/index.js
@@ -17,7 +17,8 @@ module.exports = class extends Generator {
     this.appName = this.options.arguments[0];
     this.componentName = this.options.arguments[1];
     this.isNewApp = this.options.arguments.length === 1;
-    console.log(this.options.arguments);
+    this.appNameWordArrayHyphen = this.appName.split(/(?=[A-Z])/).map((name) => name.toLowerCase() + '-');
+    this.appNameWordArrayUnderscore = this.appName.split(/(?=[A-Z])/).map((name) => name.toLowerCase() + '_');
   }
 
   paths() {
@@ -31,18 +32,16 @@ module.exports = class extends Generator {
   writing() {
     if (this.isNewApp) {
       this.fs.copyTpl(
-        this.templatePath(
-          `MyApp/index.js`
-        ),
-        this.destinationPath(`${this.appName}/index.js`), {
+        this.templatePath(`MyApp/index.js`),
+        this.destinationPath(`${this.appName}/index.js`),
+        {
           name: this.appName
         }
       );
       this.fs.copyTpl(
-        this.templatePath(
-          `MyApp/components/MyComponent${this.class}/MyComponent${this.class}.jsx`
-        ),
-        this.destinationPath(`${this.appName}/components/${this.appName}/${this.appName}.jsx`), {
+        this.templatePath(`MyApp/components/MyComponent${this.class}/MyComponent${this.class}.jsx`),
+        this.destinationPath(`${this.appName}/components/${this.appName}/${this.appName}.jsx`),
+        {
           name: this.appName
         }
       );
@@ -50,26 +49,69 @@ module.exports = class extends Generator {
         this.templatePath(
           `MyApp/components/MyComponent${this.class}/styled/MyComponent${this.class}Styled.js`
         ),
-        this.destinationPath(`${this.appName}/components/${this.appName}/styled/${this.appName}Styled.js`), {
+        this.destinationPath(
+          `${this.appName}/components/${this.appName}/styled/${this.appName}Styled.js`
+        ),
+        {
           name: this.appName
         }
       );
+
+      console.log(
+
+
+
+        `To set up your app copy the instructions below.
+
+
+        1) Paste the code below into your view where you want to load your react component.
+
+            <%= stylesheet_link_tag "common-bundle" %>
+            <%= javascript_include_tag "common-bundle" %>
+            <%= javascript_include_tag "${this.appNameWordArrayHyphen.map(word => {
+              return word;
+            }).join('')}bundle" %>
+
+            <% ${this.appNameWordArrayUnderscore.map(word => {
+              return word;
+            }).join('')}app = react_component_hash('${this.appName}App', props: {}) %>
+
+            <%= ${this.appNameWordArrayUnderscore.map(word => {
+              return word;
+            }).join('')}app["componentCss"] %>
+            <%= ${this.appNameWordArrayUnderscore.map(word => {
+              return word;
+            }).join('')}app["componentHtml"] %>
+
+
+        2) Go to '/Site/client/app/server.jsx' and paste the following line:
+
+             export ${this.appName} from './site/${this.appName}'; // eslint-disable-line no-unused-vars
+
+
+        3) Go to '/Site/client/bundles.js' and paste the following line inside module.exports:
+
+              module.exports = {
+
+                '${this.appNameWordArrayHyphen.map(word => {
+                  return word;
+                }).join('').slice(0, -1)}': bundle('./app/site/${this.appName}'),
+                ...
+              }
+      `
+      );
     } else {
       this.fs.copyTpl(
-        this.templatePath(
-          `MyComponent${this.class}/MyComponent${this.class}.jsx`
-        ),
-        this.destinationPath(`${this.componentName}/${this.componentName}.jsx`), {
+        this.templatePath(`MyComponent${this.class}/MyComponent${this.class}.jsx`),
+        this.destinationPath(`${this.componentName}/${this.componentName}.jsx`),
+        {
           name: this.componentName
         }
       );
       this.fs.copyTpl(
-        this.templatePath(
-          `MyComponent${this.class}/styled/MyComponent${this.class}Styled.js`
-        ),
-        this.destinationPath(
-          `${this.componentName}/styled/${this.componentName}Styled.js`
-        ), {
+        this.templatePath(`MyComponent${this.class}/styled/MyComponent${this.class}Styled.js`),
+        this.destinationPath(`${this.componentName}/styled/${this.componentName}Styled.js`),
+        {
           name: this.componentName
         }
       );
